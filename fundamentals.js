8756f4ce6f62be977430035985deaf792c711fb5
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-05-21 17:45:41 -0500

    Variables

---
+++ fundamentals.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const nombreInstitucion = "Escuela Politecnica Nacional";
+console.log("Uso de la palabra reservada const para declarar variables: " + nombreInstitucion);
+
+let numeroCampus = 1;
+console.log("Uso de la palabra reservada let para declarar variables: " + numeroCampus);
+
+var numeroPuertas = 5;
+console.log("Uso de la palabra reservada var para declarar variables: " + numeroPuertas);
+
+console.log()
+console.log("Nombre:" + nombreInstitucion);
+console.log("Numero de campus: " + numeroCampus);
+console.log("Numero de entradas: " + numeroPuertas);
