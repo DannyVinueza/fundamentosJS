46eae2c7d49c15c0812df2e78541ad6d46bc3ab5
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-05-21 22:15:12 -0500

    Metodos en los arreglos

--- arreglos.js
+++ arreglos.js
@@ -3,6 +3,7 @@ let utiles = ["Lapiz", "Esferos", "Borrador", "Cuadernos", "Pinturas"]
 console.log("Tienes un numero de", utiles.length, "utiles escolares.")
 
 utiles.forEach((e,i)=> console.log(`#${i+1} ${e}`))
+console.log(utiles)
+console.log("Reordenando el arreglo\n" + utiles.reverse())
 
-const newUtiles = utiles.map((e,i)=> `Util ${e} ${i}`);
-console.log(newUtiles)
+
