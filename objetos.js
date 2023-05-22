29731f96f62abdcfb3148022bf29630f88f37f45
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-05-21 21:26:27 -0500

    Agrengando y Eliminando clave y valor

--- objetos.js
+++ objetos.js
@@ -15,3 +15,15 @@ const cliente = {
 
 console.log("Nombres del cliente: " , cliente.nombre + " " + cliente.apellido)
 console.log("El precio total de ", cliente.ultimaCompra.nomProducto, " es: " , cliente.ultimaCompra.precioTotal)
+
+console.log(cliente);
+
+cliente.edad = 23;
+console.log("\n\tAgregando una nueva clave con valor edad: 20")
+console.log(cliente)
+
+console.log("\n\tEliminando la clave agrgado anteriormente")
+delete cliente.edad;
+
+console.log(cliente);
+
