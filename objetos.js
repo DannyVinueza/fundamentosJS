c66a71cbe9f94b67fc97aba2ae1cb1b517b8b606
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-05-21 21:44:14 -0500

    Destructuracion de objetos

--- objetos.js
+++ objetos.js
@@ -19,11 +19,24 @@ console.log("El precio total de ", cliente.ultimaCompra.nomProducto, " es: " , c
 console.log(cliente);
 
 cliente.edad = 23;
-console.log("\n\tAgregando una nueva clave con valor edad: 20")
+console.log("\n\tAgregando una nueva clave con valor edad: 20");
 console.log(cliente)
 
-console.log("\n\tEliminando la clave agrgado anteriormente")
+console.log("\n\tEliminando la clave agrgado anteriormente");
 delete cliente.edad;
 
 console.log(cliente);
 
+console.log("\n\tDestructuración de objetos");
+//Destructuracion de objetos asignando a variables del mismo nombre del objeto
+const {nombre, apellido, edad, ultimaCompra} = cliente;
+console.log(nombre, apellido, edad, ultimaCompra);
+//Destructuracion de objetos asignando con el mismo valor y a venta con un valor predeterminado
+//const {nombre, apellido, edad, venta = cliente.ultimaCompra} = cliente;
+
+//Destructuracion de objetos asignando con el mismo valor y sacando un valor especifico de la 
+//clave ultimaCompra
+//const {nombre, apellido, edad, ultimaCompra: {nomProducto}} = cliente;
+
+//Destructuracion de objetos asignando con el mismo valor y asignando despues de edad a nuevo un objeto nuevo
+//const {nombre, apellido, edad, ...nuevo} = cliente;
