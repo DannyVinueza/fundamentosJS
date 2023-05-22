053b8bd8de4326254ae99d09f06bd74c0fa57568
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-05-21 22:21:40 -0500

    Destructuracion de arreglos

--- arreglos.js
+++ arreglos.js
@@ -6,4 +6,18 @@ utiles.forEach((e,i)=> console.log(`#${i+1} ${e}`))
 console.log(utiles)
 console.log("Reordenando el arreglo\n" + utiles.reverse())
 
+console.log("\n\tDestructuracion de arreglos");
 
+const nuevasHerramientas = ["Martillo", "Clavos", 12, {nombre: "Carpinteria"}, ["Mesa", "Sillas"]];
+
+const [h1, h2, h3, h4, h5] = nuevasHerramientas;
+
+console.log(h1)
+console.log(h2)
+console.log(h3)
+console.log(h4)
+console.log(h5)
+
+const herramientas = nuevasHerramientas[4];
+const extraccionH = herramientas[1];
+console.log(extraccionH);
